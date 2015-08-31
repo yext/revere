5b2fb61a085dde8b41f1e1c0502da9c987c69e66
Dan Pomeroy <dcpomeroy@mac.com>
2015-08-31 18:36:44 -0400

    Database: add alert_history table

--- setup/initialize_db.sql
+++ setup/initialize_db.sql
@@ -7,3 +7,11 @@ CREATE TABLE configurations (
   config longtext,
   emails char(255)
 )
+
+CREATE TABLE alert_history (
+  id int(11) AUTO_INCREMENT PRIMARY KEY,
+  config_id int(11),
+  value int(20),
+  state char(8),
+  FOREIGN KEY (`config_id`) REFERENCES configurations (id) ON DELETE CASCADE
+)
