bc9a621dbfa132d0ddbac5d15b740b80d1124640
Dan Pomeroy <dp@yext.com>
2015-09-01 03:49:07 -0400

    Database: rename alert histories table to readings, and add columns

--- setup/initialize_db.sql
+++ setup/initialize_db.sql
@@ -5,13 +5,14 @@ use revere;
 CREATE TABLE configurations (
   id int(11) AUTO_INCREMENT PRIMARY KEY,
   config longtext,
-  emails char(255)
+  emails varchar(255)
 );
 
-CREATE TABLE alert_history (
+CREATE TABLE readings (
   id int(11) AUTO_INCREMENT PRIMARY KEY,
   config_id int(11),
-  value int(20),
-  state char(8),
+  subprobe varchar(255),
+  state int(8),
+  time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (`config_id`) REFERENCES configurations (id) ON DELETE CASCADE
 );
