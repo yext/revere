e198c1998aca7a411575524ab062529431686c00
Dan Pomeroy <dp@yext.com>
2015-09-03 16:06:12 -0400

    Databases: add foreign key to alerts table

--- setup/initialize_db.sql
+++ setup/initialize_db.sql
@@ -21,5 +21,6 @@ CREATE TABLE alerts (
   id int(11) AUTO_INCREMENT PRIMARY KEY,
   config_id int(11),
   subprobe varchar(255),
-  time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (`config_id`) REFERENCES configurations (id) ON DELETE CASCADE
 );
