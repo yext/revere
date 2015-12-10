4743d3ac6721dd84cb99c9bb7c3fbac04bce751b
Dan Pomeroy <dp@yext.com>
2015-12-10 13:39:07 -0500

    Web: move web server to separate class

---
+++ web/server/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+
+	"github.com/yext/revere"
+	"github.com/yext/revere/web"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	flag.Parse()
+	env, err := revere.BuildEnvFromFile(flag.Arg(0))
+	if err != nil {
+		fmt.Println("Unable to start web server.")
+		return
+	}
+
+	db := env.Db()
+
+	router := httprouter.New()
+
+	router.HandlerFunc("GET", "/", http.RedirectHandler("/monitors", http.StatusTemporaryRedirect).ServeHTTP)
+	router.GET("/monitors", web.MonitorsIndex(db))
+	router.GET("/monitors/:id", web.MonitorsView(db))
+	router.GET("/monitors/:id/edit", web.MonitorsEdit(db))
+	router.GET("/monitors/:id/subprobes", web.SubprobesIndex(db))
+	router.GET("/monitors/:id/subprobes/:subprobeId", web.SubprobesView(db))
+	router.ServeFiles("/static/css/*filepath", http.Dir("web/css"))
+	router.ServeFiles("/static/js/*filepath", http.Dir("web/js"))
+	router.HandlerFunc("GET", "/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "web/favicon.ico")
+	})
+}
