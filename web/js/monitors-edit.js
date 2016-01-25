a713dfe8a9c7e1f771d28409c297ad7a6496ead1
Pranjal Singh <psingh@yext.com>
2016-01-25 15:29:04 -0500

    Refactor js to follow module pattern

--- web/js/monitors-edit.js
+++ web/js/monitors-edit.js
@@ -13,131 +13,146 @@ var targets = function() {
   return t;
 }();
 
-$(document).ready(function() {
-  var $triggers = $('.trigger');
-  // Don't allow the removal of triggers if there is only one
-  if ($triggers.length == 1) {
-    $('.remove-trigger').hide();
+var monitorsEdit = function() {
+  var m = {};
+
+  m.init = function() {
+    initProbe();
+    initTriggers();
+    initForm();
+  };
+
+  var initProbe = function() {
+    var $probeType = $('#js-probe-type');
+    $probeType.change(function() {
+      var $error = $('.js-error').first().empty();
+      $error.addClass('hidden');
+      $.ajax({
+        url: '/monitors/new/probe/edit/' + encodeURIComponent($probeType.val()),
+        contentType: 'application/json; charset=UTF-8'
+      }).success(function(response) {
+        if (response.template) {
+          $('#js-probe').html(response.template);
+        }
+      }).fail(function(jqXHR, textStatus, errorThrown) {
+        $('#js-errors').html($error);
+        $error.append(jqXHR.responseText).removeClass('hidden');
+      });
+    });
+  };
+
+  var initTriggers = function() {
+    var $triggers = $('.js-trigger');
+
+    // Don't allow the removal of triggers if there is only one
+    if ($triggers.length == 1) {
+      $('.js-remove-trigger').hide();
+    }
+
+    $('#js-add-trigger').click(function(e) {
+      e.preventDefault();
+      $('.js-remove-trigger').show();
+      $triggers.first()
+        .clone()
+        .insertAfter('.js-trigger:last')
+        .find('input[type="text"]')
+        .val('');
+    });
+
+    $(document.body).on('click', '.js-remove-trigger', function(e) {
+      e.preventDefault();
+      if ($('.js-trigger').length > 1) {
+        $(this).parents('.js-trigger').remove();
+      }
+      if ($('.js-trigger').length == 1) {
+        $('.js-remove-trigger').hide();
+      }
+    });
+
+    $('.js-targetType').change(function() {
+      var $error = $('.js-error').first().empty();
+      $error.addClass('hidden');
+      $that = $(this);
+      $.ajax({
+        url: '/monitors/new/target/edit/' + encodeURIComponent($that.val()),
+        contentType: 'application/json; charset=UTF-8'
+      }).success(function(response) {
+        if (response.template) {
+          $that.parents('.js-trigger-options')
+            .next('.js-target')
+            .html(response.template);
+        }
+      }).fail(function(jqXHR, textStatus, errorThrown) {
+        $('#js-errors').html($error);
+        $error.append(jqXHR.responseText).removeClass('hidden');
+      });
+    });
+  };
+
+
+  var initForm = function() {
+    $('#js-monitor-form').submit(function(e) {
+      e.preventDefault();
+      var $form = $(this);
+      var url = $form.attr('action'),
+        data = $.extend(
+          getMonitorData(),
+          {'probe': JSON.stringify(getProbeData())},
+          {'triggers': getTriggerData()}
+        );
+      $.ajax({
+        url: url,
+        method: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json; charset=UTF-8'
+      }).success(function(response) {
+        if (response.errors) {
+          var $error = $('.js-error').first().empty();
+          $('#js-errors').html($error);
+          $.each(response.errors, function() {
+            $error.append(this + '<br/>').removeClass('hidden');
+          });
+          return;
+        }
+        if (response.redirect) {
+          window.location.replace(response.redirect);
+        } else {
+          window.location.replace('/monitors/' + data['id']);
+        }
+      }).fail(function(jqXHR, textStatus, errorThrown) {
+        var $error = $('.js-error').first().empty();
+        $('#js-errors').html($error);
+        $error.append(jqXHR.responseText).removeClass('hidden');
+      });
+    });
   }
 
-  /*
-   * Event handlers
-   */
-  $('#add-trigger').click(function(e) {
-    e.preventDefault();
-    $('.remove-trigger').show();
-    $triggers.first()
-      .clone()
-      .insertAfter('.trigger:last')
-      .find('input[type="text"]')
-      .val('');
-  });
+  // Serializing functions
+  var getMonitorData = function() {
+    return $('#js-monitor-info').find(':input').serializeObject();
+  };
 
-  $(document.body).on('click', '.remove-trigger', function(e) {
-    e.preventDefault();
-    if ($('.trigger').length > 1) {
-      $(this).parents('.trigger').remove();
-    }
-    if ($('.trigger').length == 1) {
-      $('.remove-trigger').hide();
-    }
-  });
+  var getProbeData = function() {
+    return $('#js-probe').find(':input').serializeObject();
+  };
 
-  var $probeType = $('#probe-type');
-  $probeType.change(function() {
-    var $error = $('.error').first().empty();
-    $error.addClass('hidden');
-    $.ajax({
-      url: '/monitors/new/probe/edit/' + encodeURIComponent($probeType.val()),
-      contentType: 'application/json; charset=UTF-8'
-    }).success(function(response) {
-      if (response.template) {
-        $('#probe').html(response.template);
+  var getTriggerData = function() {
+    var data = []
+    $.each($('.js-trigger'), function() {
+      var triggerOptions = $(this).find('.js-trigger-options :input').serializeObject(),
+        targetFn = targets.getSerializeFn(triggerOptions['targetType']),
+        target;
+      if (targetFn !== undefined) {
+        target = targetFn($(this).find('.js-target'));
       }
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-      $('#errors').html($error);
-      $error.append(jqXHR.responseText).removeClass('hidden');
+
+      if (target === undefined) {
+        target = $(this).find('.js-target :input').serializeObject();
+      }
+      data.push($.extend(triggerOptions, {'target':target}));
     });
-  });
+    return data;
+  };
 
-  $('.targetType').change(function() {
-    var $error = $('.error').first().empty();
-    $error.addClass('hidden');
-    $that = $(this);
-    $.ajax({
-      url: '/monitors/new/target/edit/' + encodeURIComponent($that.val()),
-      contentType: 'application/json; charset=UTF-8'
-    }).success(function(response) {
-      if (response.template) {
-        $that.parents('.form-group')
-          .next('.target')
-          .html(response.template);
-      }
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-      $('#errors').html($error);
-      $error.append(jqXHR.responseText).removeClass('hidden');
-    });
-  });
-
-  $('#monitor-form').submit(function(e) {
-    e.preventDefault();
-    var $form = $(this);
-
-    var url = $form.attr('action'),
-      data = $.extend(
-        getMonitorData(),
-        {'probe': JSON.stringify(getProbeData())},
-        {'triggers': getTriggerData()});
-
-    $.ajax({
-      url: url,
-      method: 'POST',
-      data: JSON.stringify(data),
-      contentType: 'application/json; charset=UTF-8'
-    }).success(function(response) {
-      if (response.errors) {
-        var $error = $('.error').first().empty();
-        $('#errors').html($error);
-        $.each(response.errors, function() {
-          $error.append(this + '<br/>').removeClass('hidden');
-        });
-        return;
-      }
-      if (response.redirect) {
-        window.location.replace(response.redirect);
-      } else {
-        window.location.replace('/monitors/' + data['id']);
-      }
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-      var $error = $('.error').first().empty();
-      $('#errors').html($error);
-      $error.append(jqXHR.responseText).removeClass('hidden');
-    });
-  });
-});
-
-var getMonitorData = function() {
-  return $('#monitor-info').find(':input').serializeObject();
-}
-
-var getProbeData = function() {
-  return $('#probe').find(':input').serializeObject();
-}
-
-var getTriggerData = function() {
-  var data = [];
-  $.each($('.trigger'), function() {
-    var triggerOptions = $(this).find('.trigger-options :input').serializeObject(),
-      targetFn = targets.getSerializeFn(triggerOptions['targetType']),
-      target;
-    if (targetFn !== undefined) {
-      target = targetFn($(this).find('.target'));
-    }
-
-    if (target === undefined) {
-      target = $(this).find('.target :input').serializeObject();
-    }
-    data.push($.extend(triggerOptions, {'target':target}));
-  });
-  return data;
-}
+  return m;
+}();
--- web/js/targets/email.js
+++ web/js/targets/email.js
@@ -1,50 +1,70 @@
 $(document).ready(function() {
-  // TODO(psingh): Put in appropriately named fn and remove comment
-  // Have a remove option when there are multiple emails in a trigger
-  $.each($('.trigger'), function(i) {
-    var $emails = $(this).find('.email-address');
-    $.each($(this).find('.email-address'), function(j) {
-      if (j == 0) {
-        return;
-      }
-      $(this).find('button')
-        .removeClass('add-email')
-        .addClass('remove-email')
-        .text('-');
-    });
-  });
-
-  $(document.body).on('click', '.add-email', function(e) {
-    e.preventDefault();
-    var $emailField = $('.email-address').first().clone();
-    $emailField.find('input[type="text"]').val('');
-    $emailField.find('button')
-      .removeClass('add-email')
-      .addClass('remove-email')
-      .text('-');
-    $emailField.insertAfter($(this).parents('.email-address'));
-  });
-
-  $(document.body).on('click', '.remove-email', function(e) {
-    e.preventDefault();
-    $(this).parents('.email-address').remove();
-  });
-
-  targets.addSerializeFn($('#email-target-type').val(), function(target) {
-    var emailInputs = target.find(':input').serializeObject();
-
-    // Deal with having multiple emails
-    var emails = [];
-    var emailTo = emailInputs.emailTo;
-    var replyTo = emailInputs.replyTo;
-    if (typeof emailTo === 'string') {
-      emails.push({'emailTo': emailTo, 'replyTo': replyTo});
-    } else {
-      for (var i = 0; i < emailTo.length; i++) {
-        emails.push({'emailTo': emailTo[i], 'replyTo': replyTo[i]});
-      }
-    }
-    return JSON.stringify({'emails':emails});
-  });
+  emailTarget.init();
 });
 
+var emailTarget = function() {
+  var e = {};
+
+  e.init = function() {
+    initRemove();
+    removeEmail();
+    addEmail();
+    addSerializeFn();
+  };
+
+  var initRemove = function() {
+    $.each($('.js-trigger'), function(i) {
+      var $emails = $(this).find('.js-email-address');
+      $.each($(this).find('.js-email-address'), function(j) {
+        if (j === 0) {
+          return;
+        }
+        $(this).find('button')
+          .removeClass('js-add-email')
+          .addClass('js-remove-email')
+          .text('-');
+      });
+    });
+  };
+
+  var removeEmail = function() {
+    $(document.body).on('click', '.js-remove-email', function(e) {
+      e.preventDefault();
+      $(this).parents('.js-email-address').remove();
+    });
+  };
+
+  var addEmail = function() {
+    $(document.body).on('click', '.js-add-email', function(e) {
+      e.preventDefault();
+      var $emailField = $('.js-email-address').first().clone();
+      $emailField.find('input[type="text"]').val('');
+      $emailField.find('button')
+        .removeClass('js-add-email')
+        .addClass('js-remove-email')
+        .text('-');
+      $emailField.appendTo($(this).parents('.js-target'));
+    });
+  };
+
+  var addSerializeFn = function() {
+    targets.addSerializeFn($('#js-email-target-type').val(), function(target) {
+      var emailInputs = target.find(':input').serializeObject();
+
+      // Deal with having multiple emails
+      var emails = [];
+      var emailTo = emailInputs.emailTo;
+      var replyTo = emailInputs.replyTo;
+      if (typeof emailTo === 'string') {
+        emails.push({'emailTo': emailTo, 'replyTo': replyTo});
+      } else {
+        for (var i = 0; i < emailTo.length; i++) {
+          emails.push({'emailTo': emailTo[i], 'replyTo': replyTo[i]});
+        }
+      }
+      return JSON.stringify({'emails':emails});
+    });
+  };
+
+  return e;
+}();
