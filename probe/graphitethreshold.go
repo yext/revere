5d632be987350710d7cb2237ea892d39c8a2c7d1
Austin Chu <eefi@yext.com>
2016-05-02 13:57:08 -0400

    Daemon: Implement actually querying Graphite

--- probe/graphitethreshold.go
+++ probe/graphitethreshold.go
@@ -1,12 +1,15 @@
 package probe
 
 import (
-	"math/rand"
+	"fmt"
+	"math"
 	"time"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/jmoiron/sqlx/types"
 	"github.com/juju/errors"
 
+	"github.com/yext/revere/resource"
 	"github.com/yext/revere/state"
 )
 
@@ -14,6 +17,19 @@ import (
 // Graphite metric is above or below various constant values.
 type GraphiteThreshold struct {
 	*Polling
+
+	graphiteBase string
+	expression   string
+	timeToAudit  time.Duration
+
+	thresholds      []graphiteThresholdThreshold
+	summarizeValues func(values []float64) float64
+	triggersOn      func(summaryValue, threshold float64) bool
+}
+
+type graphiteThresholdThreshold struct {
+	state     state.State
+	threshold float64
 }
 
 func newGraphiteThreshold(configJSON types.JSONText, readingsSink chan<- []Reading) (Probe, error) {
@@ -31,22 +47,118 @@ func newGraphiteThreshold(configJSON types.JSONText, readingsSink chan<- []Readi
 		return nil, errors.Mask(err)
 	}
 
+	gt.graphiteBase = fmt.Sprintf("http://%s/", config.URL)
+	gt.expression = config.Expression
+	gt.timeToAudit = time.Duration(config.TimeToAuditMilli) * time.Millisecond
+
+	// Must be in increasing severity order.
+	if config.Thresholds.Warning != nil {
+		gt.thresholds = append(gt.thresholds,
+			graphiteThresholdThreshold{state.Warning, *config.Thresholds.Warning})
+	}
+	if config.Thresholds.Error != nil {
+		gt.thresholds = append(gt.thresholds,
+			graphiteThresholdThreshold{state.Error, *config.Thresholds.Error})
+	}
+	if config.Thresholds.Critical != nil {
+		gt.thresholds = append(gt.thresholds,
+			graphiteThresholdThreshold{state.Critical, *config.Thresholds.Critical})
+	}
+
+	var ok bool
+
+	gt.summarizeValues, ok = auditFunctions[config.AuditFunction]
+	if !ok {
+		return nil, errors.Errorf("unknown audit function: %s", config.AuditFunction)
+	}
+
+	gt.triggersOn, ok = triggerIfFunctions[config.TriggerIf]
+	if !ok {
+		return nil, errors.Errorf("unknown trigger if: %s", config.TriggerIf)
+	}
+
 	return &gt, nil
 }
 
-func (gt *GraphiteThreshold) Check() []Reading {
-	// TODO(eefi): Implement.
-
-	s := state.Normal
-	if rand.Intn(4) == 0 {
-		s = state.Error
-	}
-
-	return []Reading{
-		{
-			Subprobe: "_",
-			State:    s,
-			Recorded: time.Now(),
+var (
+	auditFunctions = map[string]func([]float64) float64{
+		"avg": func(values []float64) float64 {
+			sum := float64(0)
+			count := 0
+			for _, value := range values {
+				if !math.IsNaN(value) {
+					sum += value
+					count += 1
+				}
+			}
+			return sum / float64(count)
+		},
+		"max": func(values []float64) float64 {
+			max := math.Inf(-1)
+			for _, value := range values {
+				if value > max {
+					max = value
+				}
+			}
+			return max
+		},
+		"min": func(values []float64) float64 {
+			min := math.Inf(+1)
+			for _, value := range values {
+				if value < min {
+					min = value
+				}
+			}
+			return min
 		},
 	}
+
+	triggerIfFunctions = map[string]func(float64, float64) bool{
+		"<": func(summaryValue, threshold float64) bool {
+			return summaryValue < threshold
+		},
+		"<=": func(summaryValue, threshold float64) bool {
+			return summaryValue <= threshold
+		},
+		">=": func(summaryValue, threshold float64) bool {
+			return summaryValue >= threshold
+		},
+		">": func(summaryValue, threshold float64) bool {
+			return summaryValue > threshold
+		},
+	}
+)
+
+func (gt *GraphiteThreshold) Check() []Reading {
+	g := resource.Graphite{gt.graphiteBase}
+
+	series, err := g.QueryRecent(gt.expression, gt.timeToAudit)
+	if err != nil {
+		// TODO(eefi): Include this probe's monitor's ID.
+		log.WithError(err).Error("Could not query Graphite.")
+
+		// TODO(eefi): Put err in the details.
+		return []Reading{{"_", state.Unknown, time.Now(), nil}}
+	}
+
+	if len(series) == 0 {
+		return []Reading{{"_", state.Normal, time.Now(), nil}}
+	}
+
+	recorded := series[0].End
+
+	readings := make([]Reading, len(series)+1)
+	for i, s := range series {
+		r := Reading{s.Name, state.Normal, recorded, nil}
+		summaryValue := gt.summarizeValues(s.Values)
+		for _, t := range gt.thresholds {
+			if gt.triggersOn(summaryValue, t.threshold) {
+				r.State = t.state
+			}
+		}
+		readings[i] = r
+	}
+	readings[len(series)] = Reading{"_", state.Normal, recorded, nil}
+
+	return readings
 }
