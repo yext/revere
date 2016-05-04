5a79091ac8a8f41c619d4ff5edf9cdb3ee10f8a4
Pranjal Singh <psingh@yext.com>
2016-05-04 15:59:35 -0400

    Silence: Add silence loading queries

--- db/silence.go
+++ db/silence.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/juju/errors"
@@ -17,11 +18,50 @@ type Silence struct {
 	End       time.Time
 }
 
+type MonitorSilence struct {
+	MonitorName string
+	*Silence
+}
+
 func (db *DB) LoadActiveSilencesForMonitor(monitorID MonitorID) ([]Silence, error) {
 	var silences []Silence
 	q := `SELECT * FROM pfx_silences
 	      WHERE monitorid = ? AND start <= UTC_TIMESTAMP() AND UTC_TIMESTAMP() <= end`
 	if err := db.Select(&silences, cq(db, q), monitorID); err != nil {
+		return nil, errors.Trace(err)
+	}
+	return silences, nil
+}
+
+func (db *DB) LoadMonitorSilence(id SilenceID) (*MonitorSilence, error) {
+	return loadMonitorSilence(db, id)
+}
+
+func (tx *Tx) LoadMonitorSilence(id SilenceID) (*MonitorSilence, error) {
+	return loadMonitorSilence(tx, id)
+}
+
+func loadMonitorSilence(dt dbOrTx, id SilenceID) (*MonitorSilence, error) {
+	var s MonitorSilence
+	q := `SELECT s.*, m.name AS monitorname
+		  FROM pfx_silences s
+		  JOIN pfx_monitors m USING (monitorid)
+		  WHERE s.silenceid = ?`
+	if err := dt.Get(&s, cq(dt, q), id); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, errors.Trace(err)
+	}
+	return &s, nil
+}
+
+func (db *DB) LoadMonitorSilences() ([]*MonitorSilence, error) {
+	var silences []*MonitorSilence
+	q := `SELECT s.*, m.name AS monitorname
+		  FROM pfx_silences s
+		  JOIN pfx_monitors m USING (monitorid)`
+	if err := db.Select(&silences, cq(db, q)); err != nil {
 		return nil, errors.Trace(err)
 	}
 	return silences, nil
