207bc598621345a5f8eef0c5f421dc179ac941e9
Pranjal Singh <psingh@yext.com>
2016-05-31 15:58:01 -0400

    Readings: Add load reading query

--- db/reading.go
+++ db/reading.go
@@ -17,6 +17,16 @@ type Reading struct {
 	State      state.State
 }
 
+func (db *DB) LoadReadings(subprobeID SubprobeID) ([]*Reading, error) {
+	var readings []*Reading
+	// TODO(eefi): Drop 2 suffix from table name.
+	query := `SELECT * FROM pfx_readings2 WHERE subprobeid = ? ORDER BY recorded DESC`
+	if err := db.Select(&readings, cq(db, query), subprobeID); err != nil {
+		return nil, errors.Trace(err)
+	}
+	return readings, nil
+}
+
 func (tx *Tx) InsertReading(r Reading) error {
 	// TODO(eefi): Drop 2 suffix from table name.
 	q := `INSERT INTO pfx_readings2 (subprobeid, recorded, state)
